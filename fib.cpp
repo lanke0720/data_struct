1b050fa74734038dcd14d41a47e7a207059d3cff
lanke0720 <1600977813@qq.com>
2017-12-01 23:11:21 +0800

    斐波那契升级版本问题，矩形覆盖和青蛙

--- fib.cpp
+++ fib.cpp
@@ -21,7 +21,18 @@ public:
         {
         	
         }
-     }
+		
+		
+class Solution {
+public:
+	int Fibonacci(int n) {
+	int f = 0, g = 1;
+		while(n--) {
+			g += f;
+			f = g - f;
+		}
+		return f;
+	}
 };
 大家都知道斐波那契数列，
 现在要求输入一个整数n，请你输出斐波那契数列的第n项。

3841fcf710bc4d902d6d6dd349dbc932a0ff6933
lanke0720 <1600977813@qq.com>
2018-01-05 21:51:26 +0800

    sort

---
+++ 最小K个数.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
+        vector<int> v;
+        if (k == 0 || k > input.size()) return v;
+        
+        sort(input.begin(), input.end());
+        for(int i = 0; i < k; i++)
+        {
+            v.push_back(input[i]);
+        }
+        return v;        
+    }
+};
+
+输入n个整数，找出其中最小的K个数。
+例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
